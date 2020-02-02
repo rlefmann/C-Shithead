b76c2a1796be3abc7a05a04f37421824ea9db455
Raoul Lefmann <=>
2020-02-02 20:49:28 +0100

    Add util module and die function.

---
+++ util.c
@@ -0,0 +1,14 @@
+#include "util.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void die(const char *errstr, ...) {
+	va_list ap;
+
+	va_start(ap, errstr);
+	vfprintf(stderr, errstr, ap);
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
